bdf8ffc50bd8d4c3d0e26e65b63f34df6afed9fb
Vishesh Kushwaha <82991580+vishesh11111@users.noreply.github.com>
2022-03-21 11:52:09 +0530

    Update uploade.js

--- src/middlewares/uploade.js
+++ src/middlewares/uploade.js
@@ -1,8 +1,7 @@
 const multer = require("multer");
 const path = require("path");
 const req = require("express/lib/request");
-// const expre = require("")
-//  Storage Function
+
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
     cb(null ,path.join(__dirname,"../covers"));
@@ -13,24 +12,17 @@ const storage = multer.diskStorage({
   },
 });
 
-// filter section
-// console.log(path);
 const fileFilter = (req, file, cb)=>{
 
-    // The function should call `cb` with a boolean
-    // to indicate if the file should be accepted
   
     if(file.mimetype === "image/jpeg" || file.mimetype === "image/png"){
-        // To accept the file pass `true`, like so:
+  
         cb(null, true)
     }
     else{
-        // To reject this file pass `false`, like so:
+   
         cb(null, false)
     }
-  
-    // You can always pass an error if something goes wrong:
-    // cb(new Error('I don\'t have a clue!'))
   
   };
 
